26b8299f6227eb1a3dcf70375e880d0dbb5ab500
intelburn <nerdacs@gmail.com>
2019-01-06 18:40:16 -0500

    Fixed issue to allow export of VMs with spaces in the name

--- vm-export.sh
+++ vm-export.sh
@@ -7,17 +7,18 @@ ExportDir=/home/$(whoami)/Documents
 
 #Get the group to export from the user
 echo "Which group do you want to export?"
-select group in $(vboxmanage list groups); do
-	ExportGroup=$(echo $group | sed -e 's/"//g')
+#Cut out the quotes from the group name
+select group in $(vboxmanage list groups | cut -d'"' -f 2); do
 	break
 done
 
 for VM in $(vboxmanage list vms); do
-	VMName=$(echo "$VM" | cut -d' ' -f 1 | sed -e 's/"//g')
-	VMUUID=$(echo "$VM" | cut -d' ' -f 2)
-	VMGroups=$(vboxmanage showvminfo $VMUUID | grep "Groups" | cut -d':' -f 2 | sed -e 's/^[[:space:]]*//g')
+	#cut the name from 
+	VMName=$(echo "$VM" | cut -d'"' -f 2)
+	VMUUID=$(echo "$VM" | cut -d'"' -f 3 | cut -d' ' -f 2)
+	VMGroups=$(vboxmanage showvminfo $VMUUID | grep "Groups:" | cut -d':' -f 2 | sed -e 's/^[[:space:]]*//g')
 	echo "Found VM $VMName that belongs to $VMGroups"
-	if [ $ExportGroup == $VMGroups ]; then
+	if [ $group == $VMGroups ]; then
 		ExportName=$(echo -n "$ExportDir/$VMName.ova")
 		if [ -f $ExportName ]; then
 			echo "$ExportName already exists. Overwrite y/N?"
