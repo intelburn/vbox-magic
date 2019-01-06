abd8b582c678c52c60da589ebdfbac4784abad87
intelburn <nerdacs@gmail.com>
2019-01-06 18:51:56 -0500

    Commented code

--- vm-export.sh
+++ vm-export.sh
@@ -13,22 +13,31 @@ select group in $(vboxmanage list groups | cut -d'"' -f 2); do
 done
 
 for VM in $(vboxmanage list vms); do
-	#cut the name from 
+	#cut the name of the VM out of the "s
 	VMName=$(echo "$VM" | cut -d'"' -f 2)
+	#Extract the UUID of the VM
 	VMUUID=$(echo "$VM" | cut -d'"' -f 3 | cut -d' ' -f 2)
+	#Get the groups that the VM belongs to
 	VMGroups=$(vboxmanage showvminfo $VMUUID | grep "Groups:" | cut -d':' -f 2 | sed -e 's/^[[:space:]]*//g')
 	echo "Found VM $VMName that belongs to $VMGroups"
+	#Check to see if the VM belongs to the group for export
 	if [ $group == $VMGroups ]; then
+		#Make the full absolute path and filename combo for export
 		ExportName=$(echo -n "$ExportDir/$VMName.ova")
+		#Check if the file to export exists
 		if [ -f $ExportName ]; then
+			#Prompt the user if they want to overwrite
 			echo "$ExportName already exists. Overwrite y/N?"
 			read Overwrite
 			if [ $Overwrite == 'y' ] || [ $Overwrite == 'Y' ]; then
+				#If the user wants to overwrite, then delete the conflicing file
 				rm $ExportName
 			else
+				#If the user does not want to overwrite, then skip the export
 				continue
 			fi
 		fi
+		#Actually export the VM
 		vboxmanage export $VMUUID -o $ExportName --ovf10
 	fi
 
